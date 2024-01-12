d52ae34507305580458a3c095ecb14d5753f08fe
nekoisbest <96394357+nekoisbest@users.noreply.github.com>
2024-01-12 10:28:58 +0100

    Add init shell script

---
+++ init.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Part 1: export conda to PATH, and initiate it
+# ---------------------------------------------------------
+# Add Conda to PATH permanently for bash shells
+echo "Adding Conda to PATH in .bashrc..."
+echo "export PATH=/opt/anaconda3/bin:$PATH" >> ~/.bashrc
+
+# Reload .bashrc
+source ~/.bashrc
+
+# Check Conda Version
+echo "Checking Conda version..."
+conda --version
+
+# Initiate conda
+echo "Initiating Conda..."
+conda init bash
+
+# Reload .bashrc
+source ~/.bashrc
+
+echo "Conda setup completed"
+
+# Part 2: Create conda environment for the course
+# ---------------------------------------------------------
+echo "Creating conda environment for the course..."
+conda env create -f https://raw.githubusercontent.com/ICOA-SBC/GSON-cheminformatics/master/environment.yml
+
+# Part 3: Clone the repo
+# ---------------------------------------------------------
+echo "Cloning the repo..."
+git clone https://github.com/ICOA-SBC/GSON-cheminformatics
+conda activate teachopencadd
